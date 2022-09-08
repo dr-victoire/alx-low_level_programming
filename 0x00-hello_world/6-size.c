38913fb126e4c50295fc39c696a795af961c5fe1
Victory <doctorvictory116@gmail.com>
2022-09-08 14:26:24 -0700

    cirrection for the size of data type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
- char char_type;
- int int_type;
- long int long_int_type;
- long long int long_long_int_type;
- float float_type;
+char char_type;
+int int_type;
+long int long_int_type;
+long long int long_long_int_type;
+float float_type;
 
- printf("Size of a char: %zu bytes\n", sizeof(char_type));
- printf("Size of an int: %zu bytes\n", sizeof(int_type));
- printf("Size of a long int: %zu bytes\n", sizeof(long_int_type));
- printf("Size of a long long int: %zu bytes", sizeof(long_long_int_type));
- printf("Size of a float: %zu bytes", sizeof(float_type));
+printf("Size of a char: %zu byte(s)\n", sizeof(char_type));
+printf("Size of an int: %zu byte(s)\n", sizeof(int_type));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long_int_type));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long_long_int_type));
+printf("Size of a float: %zu byte(s)", sizeof(float_type));
 
  return (0);
 }
