797b8e850e2028e252aa614a7772366887c719be
Victory <doctorvictory116@gmail.com>
2022-09-08 13:55:32 -0700

    printing with printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/** 
+* main - body of code
+*
+* Return - 0
+*/
+
+int main(void)
+{
+  char hello_string = "Programming is like building a multilingual puzzle";
+
+  puts(hello_string);
+
+  return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - defiines the body of the code
+ *
+ * Return - 0
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
