7dc7f42dcf7b34dace0997e77704813186526359
Victory <doctorvictory116@gmail.com>
2022-09-08 14:00:16 -0700

    printing with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - defiines the body of the code
  *
- * Return - 0
+ * Return: 0
  */
 int main(void)
-{
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+{ 
+ printf("with proper grammar, but the outcome is a piece of art,\n");
+ return (0);
 }
