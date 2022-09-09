3a41d14902ddad1e2cac2df3e4c2c3b740055ccc
Victory <doctorvictory116@gmail.com>
2022-09-09 12:29:34 -0700

    while loop

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 /**
- * Main - the function where all the code will be written
- * Description: This program prints all the digits of the decimal system, starting with 0
+ * main - the function where all the code will be written
+ * Description: This program prints digits starting with 0
  * Return: ALways returns 0 for success
  */
 int main(void)
 {
-  int a = 0;
-  while (a < 10) {
+int a = 0;
+
+while (a < 10)
     printf("%d\n", a);
     a++;
-  }
-  return (0);
+    
+return (0);
 }
