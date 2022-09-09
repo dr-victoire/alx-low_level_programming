692e827ec06d9cbaa66613885956ef1796f9ca97
Victory <doctorvictory116@gmail.com>
2022-09-09 15:05:11 -0700

    loop to print alphabet in lower case

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,21 +3,16 @@
 #include <stdio.h>
 
 /**
- * main - the place where all the code stays
- * Description:  prints the last number with conditions
- * Return: 0 
+ * main -the place where the code goes
+ * Description: conditional for c
+ * Return: 0 (Successful)
  */
 int main(void)
 {
-  int n;
+	int n;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  if (n > 5)
-    printf("Last digit of %d is and is greater than 5\n", n);
-  else if (n == 0)
-    printf("Last digit of %d is and is zero\n", n);
-  else 
-    printf("Last digit of %d is and is less than 5\n", n);
-  return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (
+	return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - the function where the code stays
+ * Description: prints alphabets in lowercase
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	for(int x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	return (0);
+}
