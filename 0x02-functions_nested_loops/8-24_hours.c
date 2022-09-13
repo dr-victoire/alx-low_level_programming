08388d940b3b4aff6805e51bd80af2eccf518de2
Victory <doctorvictory116@gmail.com>
2022-09-13 14:30:48 -0700

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * jack_bauer - This function prints every single minute of Jack Bauer
+ * Return: 0 (Successful)
+ */
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 0; a <= 2; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = 0; b <= 9; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
