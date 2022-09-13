a185a89d3c0cd26b34d8a2e7207ab20d5426f8c2
Victory <doctorvictory116@gmail.com>
2022-09-13 09:23:32 -0700

    comparing lowercase and uppercase letters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - This function checks if a character is lowercase or uppercase
+ * Return: 1 (Successful) and 0 (Failure)
+ */
+int _intlower(int c)
+{
+	int r;
+
+	if (97 <= r <= 122)
+	{
+		return (1);
+	} else if (65 <= r <= 90)
+	{
+		return (0);
+	}
+}
