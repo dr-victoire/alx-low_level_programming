bab2d9b69c4280ede80febae252d3803ea83d828
Victory <doctorvictory116@gmail.com>
2022-09-13 09:02:51 -0700

    function that prints the alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/** print_alphabet_x10 - This function prints the alphabet x10 in lowercase
+ * Return: 0 (Successful)
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 10; a++)
+	{
+		for (b = 'a'; b <= 'z'; b++)
+		{
+			_putchar(b);
+			_putchar('\n');
+		}
+	}
+}
