75711d20e3faa3e2b8acbee09ea40220741a947b
Victory <doctorvictory116@gmail.com>
2022-09-13 10:27:06 -0700

    comparing the signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/** print_sign - This function tells whether a number is positive or negative
+ * @n: Is the int value the function will return
+ * Return: 1 (Successful), 0 (Equality) and -1 (Failure)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
