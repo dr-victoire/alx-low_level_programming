da31d7be589fc16b8680d0f95f457dbeda839407
Victory <doctorvictory116@gmail.com>
2022-09-13 05:25:14 -0700

    printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/** main - the Entry Point where all codes will be written
+ * Description: This program will print _putchar
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this is the directory where all the projects on functions and nested loops in c will be saved
