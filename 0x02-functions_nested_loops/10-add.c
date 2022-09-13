b6a17416d53a98e6f3aeac1eaa5c8d9db33dfb9e
Victory <doctorvictory116@gmail.com>
2022-09-13 12:20:24 -0700

    try sth

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - This function adds two numbers together
+ * @a: The first int value this function takes as an argument
+ * @b: The 2nd int value argument
+ * Return: 0 (Successful)
+ */
+int add(int a, int b)
+{
+	int c;
+	
+	c = a + b;
+
+	return (c);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,6 +8,7 @@ int _islower(int c);
 int print_sign(int n);
 int print_last_digit(int l);
 void times_table(void);
+int add(int a, int b);
 void print_to_98(int n);
 
 #endif
