b665da330af0cd2345b499bc24e160506861b08e
Victory <doctorvictory116@gmail.com>
2022-09-13 14:45:41 -0700

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,13 +11,13 @@ void jack_bauer(void)
 	int c;
 	int d;
 
-	for (a = 0; a <= 2; a++)
+	for (a = '0'; a <= '2'; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = '0'; b <= '9'; b++)
 		{
-			for (c = 0; b <= 9; c++)
+			for (c = '0'; b <= '5'; c++)
 			{
-				for (d = 0; d <= 9; d++)
+				for (d = '0'; d <= '9'; d++)
 				{
 					_putchar(a);
 					_putchar(b);
