1c261e90f475d88718f7d6c783f40eb4b74133cb
Victory <doctorvictory116@gmail.com>
2022-09-13 12:02:39 -0700

    trial

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * times_table - This function prints the 9x table
+ * Return: 0 (Successful)
+ */
+void times_table(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			int c = b * 9;
+			_putchar(c);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,6 +7,7 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int print_sign(int n);
 int print_last_digit(int l);
+void times_table(void);
 void print_to_98(int n);
 
 #endif
