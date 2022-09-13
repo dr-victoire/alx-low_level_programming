2f8f08847fc9a7813d8b82ead3076ea58969ceec
Victory <doctorvictory116@gmail.com>
2022-09-13 10:17:55 -0700

    compairng whether or not a character is upper or lower case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,7 @@
 
 /**
  * _islower - This function checks if a character is lowercase or uppercase
+ * @c: Is the int type that will be passed as an argument to this function
  * Return: 1 (Successful) and 0 (Failure)
  */
 int _islower(int c)
