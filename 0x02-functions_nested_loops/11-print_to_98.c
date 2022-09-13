2fb1578425b54593270eedf1786abb452c17e01a
Victory <doctorvictory116@gmail.com>
2022-09-13 12:25:46 -0700

    try sth

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_to_98 - This function prints numbers from user input to 98
+ * @n: The int value the function takes as argument
  * Return: 0 (Successful)
  */
 void print_to_98(int n)
