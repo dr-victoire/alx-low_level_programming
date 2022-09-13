f6e31f49595bbb98af46318f308a3553de17b42e
Victory <doctorvictory116@gmail.com>
2022-09-13 13:51:43 -0700

    lets see

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - This function returns the absolute value of a number
+ * @n: This is the int value this function takes as an argument
+ * Return: 0 (Successful)
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		int a;
+		a = n * -1;
+
+		return (a);
+	}
+	else if (n == 0)
+	{
+		return (n);
+	}
+	else
+		return (n);
+}
