69b5df48698d9d9f1ba381fe56b939f897a11bce
Victory <doctorvictory116@gmail.com>
2022-09-13 05:48:02 -0700

    oopssie

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include "main.h"
 
-/** main - the Entry Point where all codes will be written
- * Description: This program will print _putchar
+/** main - entry point for all codes
+ * Description: This program prints _putchar
  * Return: 0 (Successful)
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar("_putchar\n");
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+/**
+ * Function to print stuff without using the stdio library
+ */
+int _putchar()
+{
+	return (0);
+}
