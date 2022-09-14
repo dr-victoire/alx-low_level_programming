e64b7da25affa8c5011aa8a8e96a6e4773091d19
Victory <doctorvictory116@gmail.com>
2022-09-14 03:39:46 -0700

    9x table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,17 +8,25 @@ void times_table(void)
 {
 	int a;
 	int b;
+	int c;
 
 	for (a = '0'; a <= '9'; a++)
 	{
-		for (b = '0'; b <= '9'; b++)
+		_putchar('0');
+		for (b = '1'; b <= '9'; b++)
 		{
-			_putchar(a * b);
-			if (b != '9')
+			_putchar(',');
+			_putchar(' ');
+			c = a * b;
+			if (a <= '9')
 			{
-				_putchar(',');
 				_putchar(' ');
 			}
+			else
+			{
+				_putchar((c / 10) + '0');
+			}
+			_putchar((c % 10) + '0');
 		}
 		_putchar('\n');
 	}
