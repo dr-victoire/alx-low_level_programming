ed015eaf933754b34419da8d9c7ea35f96f3a627
Victory <doctorvictory116@gmail.com>
2022-09-13 23:39:59 -0700

    xxx

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,9 +8,15 @@
  */
 void print_to_98(int n)
 {
-	while (n <= 98)
+	while ((n >= 98) || (n <= 98))
 	{
-		printf("%d, \n", n);
+		puts(n);
 		n++;
+		if (n != 98)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
+	putchar('\n');
 }
