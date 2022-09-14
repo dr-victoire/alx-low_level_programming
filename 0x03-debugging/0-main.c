c140519071cfbc6eca593b9cd1f99d3621444fdd
Victory <doctorvictory116@gmail.com>
2022-09-14 14:41:52 -0700

    debugging the positve_or_negative function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - testing the function that shows whether or not a number
+ * is positive or negative
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
