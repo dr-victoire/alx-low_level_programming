be965913c2103c2edadbc6141be54e99b46bc29d
Victory <doctorvictory116@gmail.com>
2022-09-15 13:17:10 -0700

    fixed my logic

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,20 +2,30 @@
 
 /**
  * print_square - prints a square
- * @size: the only argument
+ * @size: argument
  *
  * Return: 0 (Successful)
  */
 
 void print_square(int size)
 {
-	while (size-- > 0)
+	int a;
+	int b;
+
+	if (size > 0)
 	{
-		while (size-- > 0)
+		for (a = 0; a < size; a++)
 		{
+			for (b = 0; b < (size - 1); b++)
+			{
+				_putchar('#');
+			}
 			_putchar('#');
+			_putchar('\n');
 		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
