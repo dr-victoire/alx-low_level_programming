363ffa67cb84d8b06ca9348c31d7fec1cfed878a
Victory <doctorvictory116@gmail.com>
2022-09-15 08:38:01 -0700

    printing _ with a loop

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - prints the _ character
+ * @n: the only argument this function returns
+ *
+ * Return: 0 (Successful)
+ */
+
+void print_line(int n)
+{
+	int line;
+
+	while (line <= n)
+	{
+		_putchar('_');
+		line++;
+		_putchar('\n');
+
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+	}
+}
