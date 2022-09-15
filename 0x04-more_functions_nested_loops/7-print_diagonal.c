f438850e886c4e6bac6eb633162ed3c7c838bb87
Victory <doctorvictory116@gmail.com>
2022-09-15 10:35:56 -0700

    printing diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal n times
+ * @n: only integer this function takes
+ *
+ * Return: 0 (Successful)
+ */
+
+void print_diagonal(int n)
+{
+	while (n-- > 0)
+	{
+		_putchar('\\');
+	}
+	_putchar('\n');
+}
