53ad760cdf48299d5c98bdf8c971c9189fb21d62
Victory <doctorvictory116@gmail.com>
2022-09-15 05:28:31 -0700

    fixing betty

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,8 +3,10 @@
 /**
  * _isupper - This function checks whether an alphabet is upper
  * or lower case
+ * @c: the only argument the function takes
  * Return: 1 (Successful) or 0 (Failure)
  */
+
 int _isupper(int c)
 {
 	if (c >= 'A' || c <= 'Z')
