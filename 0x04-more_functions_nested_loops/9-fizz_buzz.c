05e96b37d0a27e88fbc17bfbf4a3d6c336bba2a4
Victory <doctorvictory116@gmail.com>
2022-09-15 12:24:54 -0700

    fixed my logic

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -22,7 +22,14 @@ int main(void)
 		}
 		else if (a % 5 == 0)
 		{
-			printf("Buzz ");
+			if (a != 100)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz");
+			}
 		}
 		else if (a % 3 == 0)
 		{
