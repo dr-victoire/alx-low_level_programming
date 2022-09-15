4ee31083baad393bb5db4daaa1d6689c571d2755
Victory <doctorvictory116@gmail.com>
2022-09-15 08:17:39 -0700

    i made an error in my programming logic. i missed an if conditional

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,16 @@ void more_numbers(void)
 	int a;
 	int b;
 
-	for (a = 0; a <= 10; a++)
+	for (a = 1; a <= 10; a++)
 	{
 		for (b = 0; b <= 14; b++)
 		{
-			_putchar(b);
+			if (b >= 10)
+			{
+				_putchar('1');
+				_putchar(b % 10 + '0');
+			}
 		}
+		_putchar('\n');
 	}
-
-	_putchar('\n');
 }
