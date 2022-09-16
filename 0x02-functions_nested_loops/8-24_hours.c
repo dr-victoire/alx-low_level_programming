906197db4f5f640b40a7de43794e3a31bd790494
Victory <doctorvictory116@gmail.com>
2022-09-16 08:08:46 -0700

    fixed the logic

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - This function prints every single minute of Jack Bauer
+ * jack_bauer - prints every minute of the day
  *
  * Return: 0 (Successful)
  */
@@ -10,71 +10,17 @@ void jack_bauer(void)
 {
 	int i;
 	int j;
-	int k;
-	int l;
 
 	for (i = 0; i < 24; i++)
 	{
-		k = i % 10;
-
-		if (i < 10)
-		{
-	 		_putchar('0');
-			_putchar(i);
-			_putchar(':');
-		}
-		else if (i < 20)
-		{
-			_putchar('1');
-			_putchar(k);
-			_putchar(':');
-		}
-		else
-		{
-			_putchar('2');
-			_putchar(k);
-			_putchar(':');
-		}
 		for (j = 0; j < 60; j++)
 		{
-			l = j % 10;
-
-			if (j < 10)
-			{
-				_putchar('0');
-				_putchar(j);
-				_putchar('\n');
-			}
-			else if (j < 20)
-			{
-				_putchar('1');
-				_putchar(l);
-				_putchar('\n');
-			}
-			else if (j < 30)
-			{
-				_putchar('2');
-				_putchar(l);
-				_putchar('\n');
-			}
-			else if (j < 40)
-			{
-				_putchar('3');
-				_putchar(l);
-				_putchar('\n');
-			}
-			else if (j < 50)
-			{
-				_putchar('4');
-				_putchar(l);
-				_putchar('\n');
-			}
-			else
-			{
-				_putchar('5');
-				_putchar(l);
-				_putchar('\n');
-			}
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n')
 		}
 	}
 }
