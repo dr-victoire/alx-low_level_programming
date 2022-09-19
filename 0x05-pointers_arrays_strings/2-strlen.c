6587fdc27296b4d5bc1ba8d0caf1f6fefc56a03e
Victory <doctorvictory116@gmail.com>
2022-09-19 06:17:28 -0700

    returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string pointer argument
+ *
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		*s++;
+	}
+
+	return (length);
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -4,3 +4,4 @@ C - POINTERS, ARRAYS AND STRINGS (README FILE)
 * This is the readme for the C Pointers, arrays and strings project
 * project 0 - (reset_to_98) resets any integer to 98
 * project 1 - swaps th two integers
+* project 2 - returns the length of a string
