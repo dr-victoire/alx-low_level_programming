7936041366da8c8d800611aefc4ecb26876759ed
Victory <doctorvictory116@gmail.com>
2022-09-20 05:33:47 -0700

    pirnting a string (half) from the second half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,35 +2,34 @@
 
 /**
  * puts_half - prints the second half of a string
- * @str: the string argument it takes
+ * @str: the string argument
  *
  * Return: 0 (Successful)
  */
 
 void puts_half(char *str)
 {
-	int a = 0;
+	int a;
 	int b;
+	int c;
 
 	while (str[a] != '\0')
 	{
 		a++;
-
-		if ((a + 1) % 2 != 0)
-		{
-			b = (a - 1) / 2;
-		}
-		else
-		{
-			b = a / 2;
-		}
-
-		b++;
 	}
 
-	for (a = b; str[a] != '\0'; a++)
+	if (a % 2 != 0)
 	{
-		_putchar(str[a]);
+		b = (a - 1) / 2;
+	}
+	else
+	{
+		b = a / 2;
+	}
+
+	for (c = b; c < a; c++)
+	{
+		_putchar(str[c]);
 	}
 
 	_putchar('\n');
