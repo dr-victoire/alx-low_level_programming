63d9ec8e8555e7ab53fac26ad74ad629b8a31dfc
Victory <doctorvictory116@gmail.com>
2022-09-21 12:39:41 -0700

    string concatenation with pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -17,12 +17,12 @@ char *_strcat(char *dest, char *src)
 	{
 		for (j = 0; src[j] != '\0'; j++)
 		{
-			*dest[i] = *src[j];
+			dest[i] = src[j];
 			i++;
 		}
 	}
 
 	dest[i] = '\0';
 
-	return (*dest);
+	return (dest);
 }
