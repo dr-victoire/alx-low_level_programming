c7b26fd8585a544d99ae2868a295ad28b01baa60
Victory <doctorvictory116@gmail.com>
2022-09-22 07:50:55 -0700

    reversing an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,15 +13,10 @@ void reverse_array(int *a, int n)
 {
 	/* initializing two variables */
 	int i;
-	int j;
-
-	/* finding the last element of
-	 * the array */
-	j = n - 1;
 
 	/* looping through the array from
 	 * last to first */
-	for (i = j; i <= 0; i--)
+	for (i = n - 1; i <= 0; i--)
 	{
 		if (i == 0)
 		{
