ab39354c73430e1a5c9f3abb9a73f6aff0685322
Victory <doctorvictory116@gmail.com>
2022-09-23 12:59:04 -0700

    printng numbers from 0 -14 10x

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,11 @@ void more_numbers(void)
 	int a;
 	int b;
 
-	for (a = 0; a < 10; a++)
+	for (a = 1; a <= 10; a++)
 	{
 		for (b = 0; b <= 14; b++)
 		{
-			if (b > 9)
+			if (b >= 10)
 			{
 				_putchar('1');
 				_putchar((b % 10) + '0');
