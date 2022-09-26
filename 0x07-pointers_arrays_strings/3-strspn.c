f3bb98ddb69050fb236c701170a0a3df1f7db992
Victory <doctorvictory116@gmail.com>
2022-09-26 14:35:37 -0700

    function that prints the length of a prefix substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -32,4 +32,6 @@ unsigned int _strspn(char *s, char *accept)
 			}
 		}
 	}
+
+	return (0);
 }
--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -9,15 +9,13 @@
 
 int main(void)
 {
-	char *s = "hello";
-	char *f;
+	char *s = "hello world";
+	char *f = "olleh";
+	unsigned int n;
 
-	f = _strchr(s, 'l');
+	n = _strspn(s, f);
 
-	if (f != NULL)
-	{
-		printf("%s\n", f);
-	}
+	printf("%u\n", n);
 
 	return (0);
 }
