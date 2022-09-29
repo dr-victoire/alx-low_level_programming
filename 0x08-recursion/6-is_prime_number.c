90029871ab9747ab919c1a6c929a398f8e7c6464
Victory <doctorvictory116@gmail.com>
2022-09-29 06:09:16 -0700

    function that checks whether or not an integer is a prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+int is_prime_number(int n);
+int check_for_prime(int n, int i);
+
+/**
+ * is_prime_number - checkswhether or not an integer is prime
+ * @n: the integer argument
+ *
+ * Return: 1 if integer is prime and 0 if it's not prime
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 0)
+	{
+		return (0);
+	}
+	else if (n == 1 || n == 2)
+	{
+		return (1);
+	}
+	else
+	{
+		return (check_for_prime(n, 2));
+	}
+}
+
+/**
+ * check_for_prime - checks whether or not an integer is prime
+ * @n: the integer
+ * @i: the modulator to check
+ *
+ * Return: 0 if not prime or 1 if prime
+ */
+
+int check_for_prime(int n, int i)
+{
+	if (i == n)
+	{
+		return (1);
+	}
+	else if ((n % i) == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (check_for_prime(n, ++i));
+	}
+}
--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -11,11 +11,17 @@ int main(void)
 {
 	int r;
 
-	r = _sqrt_recursion(1);
+	r = is_prime_number(1);
 	printf("%d\n", r);
-	r = _sqrt_recursion(1024);
+	r = is_prime_number(1024);
 	printf("%d\n", r);
-	r = _sqrt_recursion(17);
+	r = is_prime_number(17);
+	printf("%d\n", r);
+	r = is_prime_number(32);
+	printf("%d\n", r);
+	r = is_prime_number(144);
+	printf("%d\n", r);
+	r = is_prime_number(31);
 	printf("%d\n", r);
 
 	return (0);
