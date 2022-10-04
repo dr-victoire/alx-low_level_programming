5cce4bf74d4e12773b26fd3e41baa9aa37a1fcc6
Victory <doctorvictory116@gmail.com>
2022-10-04 11:15:08 -0700

    2d array with DMA

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - gives a 2D array of integers
+ * @width: array's rows
+ * @height: arrray's collumns
+ *
+ * Return: Pointer to the array of integers
+ * On Failure, return NULL
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int *grid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = malloc(sizeof(*grid));
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < heigth; j++)
+		{
+			grid = grid[i][j];
+		}
+	}
+
+	return (grid);
+}
