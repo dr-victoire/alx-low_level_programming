067e758be4117f30c96315ed5586a09dd8913c5d
Victory <doctorvictory116@gmail.com>
2022-10-04 11:03:18 -0700

    string concatenation with DMA

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,42 +14,34 @@ char *str_concat(char *s1, char *s2)
 {
 	unsigned int i, j, len1 = 0, len2 = 0, len;
 	char *result;
+
 	if (s1 != NULL)
 	{
 		i = 0;
 
-		while (s1[i] != '\0')
+		while (s1[i++] != '\0')
 		{
 			len1++;
-			i++;
 		}
 	}
 	if (s2 != NULL)
 	{
 		i = 0;
 
-		while (s2[i] != '\0')
+		while (s2[i++] != '\0')
 		{
 			len2++;
-			i++;
 		}
 	}
 	len = len1 + len2;
-
 	result = malloc(sizeof(*result) * len + 1);
 
 	if (result == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; i < len1; i++)
-	{
 		result[i] = s1[i];
-	}
 	for (j = 0; j < len2; j++)
-	{
 		result[j] = s2[j];
-	}
 	result[len] = '\0';
 
 	return (result);
