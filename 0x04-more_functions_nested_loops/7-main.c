0fc5493ea501e869d4956d43c781fca7bc29204d
Victory <doctorvictory116@gmail.com>
2022-10-05 03:17:43 -0700

    printing diagonals

---
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - checks code
+ *
+ * Return: Nothing
+ */
+
+int main(void)
+{
+	print_diagonal(0);
+	print_diagonal(2);
+	print_diagonal(10);
+	print_diagonal(-4);
+	print_diagonal(100);
+
+	return(0);
+
+}
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,9 +9,25 @@
 
 void print_diagonal(int n)
 {
-	while (n-- > 0)
+	int i, j;
+
+	if (n <= 0)
 	{
-		_putchar('\\');
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				if (j < i)
+					_putchar(' ');
+			}
+
+			_putchar('\n');
+		}
 	}
 }
