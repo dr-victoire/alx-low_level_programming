f020e2d8d3c288ff700dbb61413488c28b1aa99a
Victory <doctorvictory116@gmail.com>
2022-10-06 03:40:09 -0700

    corrected the error of omiting a parenthesis

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -32,8 +32,8 @@ void print_times_tables(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(prod / 10) + '0');
-					_putchar(prod % 10) + '0');
+					_putchar((prod / 10) + '0');
+					_putchar((prod % 10) + '0');
 				} else if (prod <= 100)
 				{
 					_putchar(',');
