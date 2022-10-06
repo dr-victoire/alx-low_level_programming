179199ba251eea66c9feacce879589b9d7ca1c05
Victory <doctorvictory116@gmail.com>
2022-10-06 03:46:06 -0700

    printing times table from 0 - n

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,7 @@
  * Return: Nothing
  */
 
-void print_times_tabl(int n)
+void print_times_table(int n)
 {
 	int i, j, prod;
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -15,4 +15,4 @@ int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif
+#endif /* MAIN_H */
