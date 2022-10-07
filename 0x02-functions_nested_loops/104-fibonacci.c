4278ca6ff27d76cc9e38aaa854baaac0d35704e0
Victory <doctorvictory116@gmail.com>
2022-10-07 08:13:25 -0700

    printing the first 98 numbers in the fibonacci series

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,44 +9,43 @@
 int main(void)
 {
 	int i;
-	unsigned long int a = 0;
-	unsigned long int b = 1;
-	unsigned long int sum;
-	unsigned long int a1, a2, b1, b2, c1, c2;
+	unsigned long fib1 = 0, fib2 = 0, sum;
+	unsigned long fib1_half1,fib1_half2, fib2_half1, fib2_half2;
+	unsigned long half1. half2;
 
 	for (i = 0; i < 92; i++)
 	{
-		sum = a + b;
+		sum = fib1 + fib2;
 		printf("%lu, ", sum);
-		a = b;
-		b = sum;
+		fib1 = fib2;
+		fib2 = sum;
 	}
 
-	a1 = a / 10000000000;
-	a2 = b / 10000000000;
-	b1 = a % 10000000000;
-	b2 = b % 10000000000;
+	fib1_half1 = fib1 / 10000000000;
+	fib2_half1 = fib2 / 10000000000;
+	fib1_half2 = fib1 % 10000000000;
+	fib2_half2 = fib2 % 10000000000;
 
 	for (i = 93; i < 99; i++)
 	{
-		c1 = a1 + a2;
-		c2 = b1 + b2;
+		half1 = fib1_half1 + fib2_half1;
+		half2 = fib1_half2 + fib2_half2;
 
-		if (c2 > 9999999999)
+		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
-			c1 += 1;
-			c2 %= 10000000000;
+			half1 += 1;
+			half2 %= 10000000000
 		}
 
-		printf("%lu%lu", c1, c2);
+		printf("%lu%lu", half1, half2);
 
 		if (i != 98)
 			printf(", ");
 
-		a1 = b1;
-		a2 = b2;
-		b1 = c1;
-		b2 = c2;
+		fib1_half1 = fib2_half1;
+		fib1_half2 = fib2_half2;
+		fib2_half1 = half1;
+		fib2_half2 = half2;
 	}
 
 	printf("\n");
