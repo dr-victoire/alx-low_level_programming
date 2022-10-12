2bf66c2c89b80bf4afca552109688ab88ff93a5a
Victory <doctorvictory116@gmail.com>
2022-10-12 14:15:46 -0700

    acting upon an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function on each element of an array
+ * @array: the array
+ * @size: array size
+ * @action: the function pointer to execute
+ *
+ * Return: Nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
