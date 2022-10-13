5fc45911820538b5a81cade16f01c8f10e727372
Victory <doctorvictory116@gmail.com>
2022-10-13 12:18:28 -0700

    minor changes

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * main - checks code
+ *
+ * Return: 0 (Successful)
+ */
+
+int main(void)
+{
+	print_strings(",", 2, "Jay", "Django");
+	return (0);
+}
