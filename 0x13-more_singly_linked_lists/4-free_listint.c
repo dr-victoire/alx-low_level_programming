aed47f226d4f0d0084e5d8b6f39f7d0b802e0563
Victory <doctorvictory116@gmail.com>
2022-10-25 08:43:00 -0700

    a function that frees a linkedl ist

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: the head
+ *
+ * Return: Nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *h;
+
+	while (head != NULL)
+	{
+		h = head->next;
+		free(h->n);
+		free(head);
+		head = h;
+	}
+}
